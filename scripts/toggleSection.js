c74821fcdbe4021c31fdf2d74d794ee49527730f
Edmund Mulligan <edmund@edmundmulligan.name>
2025-11-30 20:52:22 +0000

    Refactor CSS and JavaScript class names for consistency; update event listener logic in toggleSection script

--- scripts/toggleSection.js
+++ scripts/toggleSection.js
@@ -5,7 +5,7 @@
  * License    : MIT License (see license-and-credits.html page)
  * Description:
  *     This scripts toggles the visibility of sections in a webpage.
- *     It adds event listeners to elements with the class 'magicVisible'.
+ *     It adds event listeners to elements with the class 'magic-visible'.
  *     When clicked, it toggles the visibility of the corresponding 
  *     section and changes the class of the clicked element 
  *     'magicInvisible'.
@@ -15,12 +15,12 @@ function toggleSection(elementId, event) {
     const element = document.getElementById(elementId);
     if (element) {
         element.classList.toggle('hidden');
-        if (event.currentTarget.classList.contains('magicVisible')) {
-            event.currentTarget.classList.remove('magicVisible');
+        if (event.currentTarget.classList.contains('magic-visible')) {
+            event.currentTarget.classList.remove('magic-visible');
             event.currentTarget.classList.add('magicInvisible');
         } else {
             event.currentTarget.classList.remove('magicInvisible');
-            event.currentTarget.classList.add('magicVisible');
+            event.currentTarget.classList.add('magic-visible');
         }
     }
 }
